0449289ebea799117c3f0b6da8defcddbc0eea51
Nishant Rodrigues <nishnash54@gmail.com>
2018-02-09 12:59:48 +0530

    Plotting fire

---
+++ plot_fire.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def create_plots(data):
+    xdata = []
+    ydata = []
+    y = data.shape[0]
+    x = data.shape[1]
+    print x, y
+    for each in data:
+        for i in range(x):
+            if each[i]==1:
+                xdata.append(i)
+                ydata.append(y)
+        y -= 1
+    print xdata, ydata
+    plt.plot(xdata, ydata, 'ro')
+    plt.show()
