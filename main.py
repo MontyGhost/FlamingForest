c8dea93cb39fe41a537d4db4b014e778ef2cefbe
Nishant Rodrigues <nishnash54@gmail.com>
2018-02-11 23:46:47 +0530

    East wind with variable size

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from random_direction import generate_random_value
 
 def create_forest(num, wind):
     forest = np.zeros((num, num))
-    x  = y = num/2
+    x  = y = num//2
     forest[x, y] = 1
     lst = []
-    for i in range(5):
+    for i in range(num//2):
         value = generate_random_value(wind)
         lst.append(value)
         y += 1
@@ -16,6 +16,6 @@ def create_forest(num, wind):
     return forest
 
 if __name__ == "__main__":
-    forest_fire = create_forest(11, "East")
-    print forest_fire
+    forest_fire = create_forest(101, "East")
+    #print forest_fire
     create_plots(forest_fire)
