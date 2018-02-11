8bfaa1d5b748d0bcfb4d8539ff6a2c565a864f6d
Nishant Rodrigues <nishnash54@gmail.com>
2018-02-11 23:46:07 +0530

    Colour plot

--- plot_fire.py
+++ plot_fire.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def create_plots(data):
-    xdata = []
+    '''xdata = []
     ydata = []
     y = data.shape[0]
     x = data.shape[1]
@@ -13,6 +13,7 @@ def create_plots(data):
                 xdata.append(i)
                 ydata.append(y)
         y -= 1
-    print xdata, ydata
-    plt.plot(xdata, ydata, 'ro')
+    print xdata, ydata'''
+    #plt.plot(xdata, ydata, 'ro')
+    plt.matshow((data+1)%2, fignum=None, cmap=plt.cm.RdYlGn)
     plt.show()
