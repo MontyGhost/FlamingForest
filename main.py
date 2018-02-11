064a3bff91f9738d846e7b26dcf16297d641f682
Nishant Rodrigues <nishnash54@gmail.com>
2018-02-12 00:16:20 +0530

    Custom input

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
 import numpy as np
 from plot_fire import create_plots
-from random_direction import generate_random_value
-
-def create_forest(num, wind):
-    forest = np.zeros((num, num))
-    x  = y = num//2
-    forest[x, y] = 1
-    lst = []
-    for i in range(num//2):
-        value = generate_random_value(wind)
-        lst.append(value)
-        y += 1
-        x -= value
-        forest[x, y] = 1
-    return forest
+from forest import create_forest
 
 if __name__ == "__main__":
-    forest_fire = create_forest(101, "East")
-    #print forest_fire
+    num = int(input('Enter size of forest : '))
+    direction = raw_input('Enter wind direction (None of no wind) : ').strip()
+    if direction == "None":
+        forest_fire = create_forest(num)
+    else:
+        forest_fire = create_forest(num, direction)
     create_plots(forest_fire)
